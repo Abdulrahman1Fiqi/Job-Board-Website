9445614d87db0a99be96e17a8a6bb746e68a15ea
Abdulrahman Al_Fiqi <abdulrahmanhmh47@gmail.com>
2026-02-27 07:55:44 +0200

    Company : List Page  by handling company controller and view

---
+++ job-backoffice/app/Http/Requests/CompanyCreateRequest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CompanyCreateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255|unique:companies,name',
+            'address' => 'required|string|max:255',
+            'industry' => 'required|string|max:255',
+            'website' => 'nullable|string|url|max:255',
+        ];
+    }
+
+    public function messages():array
+    {
+        return [
+            'name.required'=>'The Category name is required.',
+            'name.unique'=>'The Category name has already been taken.',
+            'name.max'=>'The Category name must be less than 255 characters.',
+            'name.string'=>'The Category name must be a string.',
+            'address.required'=>'The Category name is required.',
+            'address.max'=>'The Category name must be less than 255 characters.',
+            'address.string'=>'The Category name must be a string.',
+            'industry.required'=>'The Category name is required.',
+            'industry.max'=>'The Category name must be less than 255 characters.',
+            'industry.string'=>'The Category name must be a string.',
+            'website.url'=>'The company website must be a valid URL.',
+            'website.max'=>'The company website must be less than 255 characters.',
+            'website.string'=>'The company website must be a string.',
+        ];
+    }
+}
