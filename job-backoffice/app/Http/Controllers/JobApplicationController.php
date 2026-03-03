5b0580141a2378170e8dbc74700fa6b69510a54d
Abdulrahman Al_Fiqi <abdulrahmanhmh47@gmail.com>
2026-03-03 07:09:00 +0200

    Job Application : Show Page

--- job-backoffice/app/Http/Controllers/JobApplicationController.php
+++ job-backoffice/app/Http/Controllers/JobApplicationController.php
@@ -45,7 +45,8 @@ class JobApplicationController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $jobApplication = JobApplication::findOrFail($id);
+        return view('job-application.show',compact('jobApplication'));
     }
 
     /**
