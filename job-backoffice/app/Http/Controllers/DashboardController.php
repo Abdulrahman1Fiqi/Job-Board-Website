c4cc28c34e42af090bc49b259ec45c83eda73eb4
Abdulrahman Al_Fiqi <abdulrahmanhmh47@gmail.com>
2026-03-05 06:43:47 +0200

    Company Owner Logic handling the analytics and the dashboard view and controller

--- job-backoffice/app/Http/Controllers/DashboardController.php
+++ job-backoffice/app/Http/Controllers/DashboardController.php
@@ -12,7 +12,26 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        // Last 30 days active users (job-seeker role)
+        if(auth()->user()->role =='admin'){
+            $analytics = $this->adminDashboard();
+        }else{
+            $analytics = $this->companyOwnerDashboard();
+        }
+
+
+        return view('dashboard.index',compact(['analytics']));
+    }
+
+
+
+
+
+
+
+
+
+    private function adminDashboard(){
+         // Last 30 days active users (job-seeker role)
         $activeUsers = User::where('last_login_at','>=',now()->subDays(30))
         ->where('role','job-seeker')->count();
 
@@ -22,11 +41,7 @@ class DashboardController extends Controller
         // Total applications (not deleted)
         $totalApplications = JobApplication::whereNull('deleted_at')->count();
 
-        $analytics = [
-            'activeUsers'=>$activeUsers,
-            'totalJobs'=>$totalJobs,
-            'totalApplications'=>$totalApplications,
-        ];
+        
 
         // Most applied jobs
         $mostAppliedJobs = JobVacancy::withCount('jobApplications as totalCount')
@@ -52,8 +67,76 @@ class DashboardController extends Controller
             return $job;
         });
 
+        $analytics = [
+            'activeUsers'=>$activeUsers,
+            'totalJobs'=>$totalJobs,
+            'totalApplications'=>$totalApplications,
+            'mostAppliedJobs'=>$mostAppliedJobs,
+            'conversionRates'=>$conversionRates
+        ];
+
+        return $analytics;
+    }
+
+    private function companyOwnerDashboard()
+    {
+        $company = auth()->user()->company;
+
+        // Filter active users by applying to jobs of the company
+        $activeUsers = User::where('last_login_at','>=',now()->subDays(30))
+        ->where('role','job-seeker')
+        ->whereHas('jobApplications',function($query) use ($company){
+            $query->whereIn('jobVacancyId',$company->jobVacancies->pluck('id'));
+        })
+        ->count();
 
 
-        return view('dashboard.index',compact(['analytics','mostAppliedJobs','conversionRates']));
+        // Total jobs of the company
+        $totalJobs =$company->jobVacancies->count();
+
+        // Total applications of the company
+        $totalApplications = JobApplication::whereIn('jobVacancyId',$company->jobVacancies->pluck('id'))->count();
+
+        // Most applied jobs of the company
+        $mostAppliedJobs = JobVacancy::withCount('jobApplications as totalCount')
+        ->whereIn('id',$company->jobVacancies->pluck('id'))
+        ->whereNull('deleted_at')
+        ->orderByDesc('totalCount')
+        ->limit(5)
+        ->get(); 
+
+
+        // Conversion rates
+        $conversionRates = JobVacancy::withCount('jobApplications as totalCount')
+        ->whereIn('id',$company->jobVacancies->pluck('id'))
+        ->having('totalCount','>',0)
+        ->orderByDesc('totalCount')
+        ->limit(5)
+        ->get()
+        ->map(function($job){
+            if($job->viewCount > 0){
+                 $job->conversionRate = round($job->totalCount / $job->viewCount * 100,2);
+            }else{
+                  $job->conversionRate = 0;
+            }
+           
+            return $job;
+        });
+
+
+
+    $analytics = [
+        'activeUsers'=>$activeUsers,
+        'totalJobs'=>$totalJobs,
+        'totalApplications'=>$totalApplications,
+        'mostAppliedJobs'=>$mostAppliedJobs,
+        'conversionRates'=>$conversionRates,
+    ];
+
+    return $analytics;
+
+
+
+
     }
 }
