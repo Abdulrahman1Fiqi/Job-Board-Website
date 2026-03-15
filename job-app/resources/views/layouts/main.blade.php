47e4a3bb8926afe57604311c25d8466f9112dcbf
Abdulrahman Al_Fiqi <abdulrahmanhmh47@gmail.com>
2026-03-15 09:13:58 +0200

    Adding a Favicon

--- job-app/resources/views/layouts/main.blade.php
+++ job-app/resources/views/layouts/main.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>{{ $title ?? 'HireHub' }}</title>
-
+    <link rel="icon" type="image/png" href="{{ asset('images/logo1.png') }}">
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
